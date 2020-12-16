63ad9e82423c410468b8694d32686a03c9ac752b
sangchao <947934454@qq.com>
2020-12-16 10:01:57 +0800

    箭头

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import qs from 'qs'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.prototype.$qs = qs;
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
