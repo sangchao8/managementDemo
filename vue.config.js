489e874ed788bade7214f10560ab18a392b654d3
sangchao <947934454@qq.com>
2020-12-18 09:32:26 +0800

    菜单优化

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
    /* assetsDir:'assets',
     lintOnSave:false,*/
-  /*  devServer: {
+    /*devServer: {
         open: true,
         host: 'localhost',
         port: 8080,
